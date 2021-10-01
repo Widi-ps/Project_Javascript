42c5d80794a440b4a12a37ad103967a9fa353374
widi-ps <widyapujisaputro@gmail.com>
2021-10-01 21:02:03 +0700

    adding style

---
+++ quiz-app/script.js
@@ -0,0 +1,33 @@
+const quizData = [
+  {
+    question: "How old is queen elizabeth?",
+    a: "93",
+    b: "94",
+    c: "95",
+    d: "96",
+    correct: "c",
+  },
+  {
+    question: "What is the best programming language?",
+    a: "Javascript",
+    b: "Java",
+    c: "Phyton",
+    correct: "a",
+  },
+  {
+    question: "What does HTML stand for?",
+    a: "Hypertext Markup Language",
+    b: "Cascading Style Sheet",
+    c: "Javascript Object Notation",
+    d: "Helicopters Terminals Motorboats Lamborginis",
+    correct: "a"
+  },
+  {
+    question: "Who is the President Of Indonesia?",
+    a: "Luhut Binsar Panjaitan",
+    b: "Puan Maharani",
+    c: "Joko Widodo",
+    d: "Jusuf Kalla",
+    correct: "c"
+  }
+];
